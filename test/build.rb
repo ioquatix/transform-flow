0d97b65fdb854da36b4c2b29966ad4652172b87f
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2013-09-28 02:20:29 +1200

    Remove hard coded Cocoa linkflag.

--- test/build.rb
+++ test/build.rb
@@ -3,10 +3,6 @@ compile_executable("transform-flow-test-runner") do
 	def source_files(environment)
 		FileList[root, "**/*.cpp"]
 	end
-	
-	configure do
-		append linkflags ["-framework", "Cocoa"]
-	end
 end
 
 copy_files do
